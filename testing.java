9eb50e6d075817e778c4373e79c9ce1e01f92910
C10Blank <ql1521i75@gmail.com>
2025-09-02 21:05:24 -0700

    fix

--- testing.java
+++ testing.java
@@ -2,7 +2,7 @@ public class testing {
     public class BrokenCode {
         public static void main(String[] args) {
             int[] numbers = {1, 2, 3, 4, 5};
-            for (int i = 0; i <= numbers.length; i++) { // Off-by-one error
+            for (int i = 0; i < numbers.length; i++) { // Off-by-one error
                 System.out.println(numbers[i]);
             }
         }
